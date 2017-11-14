05f617a74c47c130cb1c9854506d2df9259ee586
Andreas Radke <radkeandreas@gmail.com>
2017-11-14 23:01:27 +0100

    little naming changes, Münchens to München, the Madrid geojson file was for neighborhoods, we still need the districts file

--- meetup/cities.py
+++ meetup/cities.py
@@ -1,3 +1,3 @@
 cities = {"London": "gb", "Barcelona": "es", "Berlin": "de",
           "Paris": "fr", "Madrid": "es", "Hamburg": "de",
-          "New York": ("us", "NY"), "Brussels": "be", "Munich": "de", "Sydney": "au", "Vancouver": "ca"}
+          "New York": ("us", "NY"), "Brussels": "be", "München": "de", "Sydney": "au", "Vancouver": "ca", "Hong Kong": "hk"}
